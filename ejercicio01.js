a3023794673cea113482719a67525e03182b75b3
emmanuel-hybridgeEducation <jesus2409016@hybridge.education>
2025-12-03 12:18:43 -0600

    Entrega de las actividades.

--- ejercicio01.js
+++ ejercicio01.js
@@ -1,8 +1,10 @@
 // Ejercicio 1: Declaración de Función
-// Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. La función debe retornar esta nueva variable que contiene tu nombre
+// Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. 
+// La función debe retornar esta nueva variable que contiene tu nombre
 
 function obtenerNombre() {
-    // Escribe tu código aquí
+    let nombre = "Jesús Emmanuel Morfín Olmos"
+    return nombre;
 }
 
 // No modificar.
